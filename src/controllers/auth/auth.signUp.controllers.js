59e9061514a8acbf29a2ff8ffb84489eab347aa6
Pedro Jose <bermudezlopezpedrojose@gmail.com>
2023-05-02 20:49:38 -0500

    module auth complet

--- src/controllers/auth/auth.signUp.controllers.js
+++ src/controllers/auth/auth.signUp.controllers.js
@@ -56,47 +56,42 @@ controllerAuth.signUpEmployee = async (req, res) => {
   let store = (req.body.idStore) ? req.body.idStore : null;
   let codePermission = 2;
 
-  await connectionDb.query(
-    "SELECT * FROM employee WHERE email_employee = ?",
-    [emailUser],
-    async (err, rows) => {
-      if (!err) {
-        if (rows.length > 0) {
-          return res.status("202").send({
-            mensaje: "El usuario ya existe",
-            userName: rows[0].name_employee,
-          });
-        } else if (rows.length === 0) {
-          await connectionDb.query(
-            "INSERT INTO employee SET ?", {
-            name_employee: nameUser,
-            email_employee: emailUser,
-            password_employee: passwordHash,
-            state_employee: 'asset',
-            id_permissions_employee: codePermission,
-            id_store: store
-          },
-            (err, rows) => {
-              if (err) {
-                return res.status("202").send({
-                  mensaje: "Error al registrar el usuario",
-                  error: err,
-                });
-              } else {
-                return res.status("200").send({
-                  mensaje: "Usuario registrado con exito",
-                });
-              }
-            }
-          );
-        }
-      } else {
+  await connectionDb.query("SELECT * FROM employee WHERE email_employee = ?", [emailUser], async (err, rows) => {
+    if (!err) {
+      if (rows.length > 0) {
         return res.status("202").send({
-          mensaje: "Error al registrar el usuario",
-          error: err,
+          mensaje: "El usuario ya existe",
+          userName: rows[0].name_employee
         });
+      } else if (rows.length === 0) {
+        await connectionDb.query("INSERT INTO employee SET ?", {
+          name_employee: nameUser,
+          email_employee: emailUser,
+          password_employee: passwordHash,
+          state_employee: 'asset',
+          id_permissions_employee: codePermission,
+          id_store: store
+        }, (err, rows) => {
+          if (err) {
+            return res.status("202").send({
+              mensaje: "Error al registrar el usuario",
+              error: err
+            });
+          } else {
+            return res.status("200").send({
+              mensaje: "Usuario registrado con exito"
+            });
+          }
+        }
+        );
       }
+    } else {
+      return res.status("202").send({
+        mensaje: "Error al registrar el usuario",
+        error: err
+      });
     }
+  }
   );
 };
 
@@ -107,46 +102,40 @@ controllerAuth.signUpCustomer = async (req, res) => {
   let passwordHash = await encrypted.encryptPassword(passwordUser);
   let codePermission = 3;
 
-
-  await connectionDb.query(
-    "SELECT * FROM customer WHERE id_customer = ?",
-    [idUser],
-    async (err, rows) => {
-      if (!err) {
-        if (rows.length > 0) {
-          return res.status("202").send({
-            mensaje: "El usuario ya existe",
-            userName: rows[0].name_customer,
-          });
-        } else if (rows.length === 0) {
-          await connectionDb.query(
-            "INSERT INTO customer SET ?", {
-            name_customer: nameUser,
-            email_customer: emailUser,
-            password_customer: passwordHash,
-            id_permissions_customer: codePermission,
-          },
-            (err, rows) => {
-              if (err) {
-                return res.status("202").send({
-                  mensaje: "Error al registrar el usuario",
-                  error: err,
-                });
-              } else {
-                return res.status("200").send({
-                  mensaje: "Usuario registrado con exito",
-                });
-              }
-            }
-          );
-        }
-      } else {
+  await connectionDb.query("SELECT * FROM customer WHERE id_customer = ?", [idUser], async (err, rows) => {
+    if (!err) {
+      if (rows.length > 0) {
         return res.status("202").send({
-          mensaje: "Error al registrar el usuario",
-          error: err,
+          mensaje: "El usuario ya existe",
+          userName: rows[0].name_customer
         });
+      } else if (rows.length === 0) {
+        await connectionDb.query("INSERT INTO customer SET ?", {
+          name_customer: nameUser,
+          email_customer: emailUser,
+          password_customer: passwordHash,
+          id_permissions_customer: codePermission
+        }, (err, rows) => {
+          if (err) {
+            return res.status("202").send({
+              mensaje: "Error al registrar el usuario",
+              error: err
+            });
+          } else {
+            return res.status("200").send({
+              mensaje: "Usuario registrado con exito"
+            });
+          }
+        }
+        );
       }
+    } else {
+      return res.status("202").send({
+        mensaje: "Error al registrar el usuario",
+        error: err
+      });
     }
+  }
   );
 };
 
--- src/controllers/auth/auth.singIn.controllers.js
+++ src/controllers/auth/auth.singIn.controllers.js
@@ -9,99 +9,96 @@ dotenv.config();
 const controllerAuth = {};
 
 controllerAuth.singIn = async (req, res) => {
-  let email = req.body.emailUser;
-  let password = req.body.passwordUser;
+  let emailUser = req.body.email;
+  let passwordUser = req.body.password;
 
-
-  connectionDb.query(
-    "SELECT * FROM administrator WHERE email_admin = ?", [email], async (err, rows) => {
-      if (!err) {
-        if (rows.length > 0) {
-          let passwordUser = rows[0].password_admin;
-          let passwordCompare = await encrypted.matchPassword(password, passwordUser);
-          if (passwordCompare) {
-            let user = {
-              email: rows[0].email_admin,
-              permission: rows[0].id_permissions_admin
-            };
-            let accessToken = await jwt.generateAccessToken(user);
-            return res.status("200").send({
-              mensaje: "Usuario logueado con exito",
-              accessToken
-            });
-          } else {
-            return res.status("202").send({
-              mensaje: "Contraseña incorrecta"
-            });
-          }
-        } else if (rows.length === 0) {
-          await connectionDb.query("SELECT * FROM employee WHERE email_employee = ?", [email], async (err, rows) => {
-            if (!err) {
-              if (rows.length > 0) {
-                let passwordUser = rows[0].password_employee;
-                let passwordCompare = await encrypted.matchPassword(password, passwordUser);
-                if (passwordCompare) {
-                  let user = {
-                    email: rows[0].email_employee,
-                    permission: rows[0].id_permissions_employee
-                  };
-                  let accessToken = await jwt.generateAccessToken(user);
-                  return res.status("200").send({
-                    mensaje: "Se logueo",
-                    accessToken
-                  });
-                } else {
-                  return res.status("202").send({
-                    mensaje: "Contraseña incorrecta"
-                  });
-                }
-              } else if (rows.length === 0) {
-                await connectionDb.query("SELECT * FROM customer WHERE email_customer = ?", [email], async (err, rows) => {
-                  if (!err) {
-                    if (rows.length > 0) {
-                      let passwordUser = rows[0].password_customer;
-                      let passwordCompare = await encrypted.matchPassword(password, passwordUser);
-                      if (passwordCompare) {
-                        let user = {
-                          email: rows[0].email_customer,
-                          permission: rows[0].id_permissions_customer
-                        };
-                        let accessToken = await jwt.generateAccessToken(user);
-                        return res.status("200").send({
-                          mensaje: "Usuario logueado",
-                          accessToken
-                        });
-                      }
-                    } else if (rows.length === 0) {
-                      return res.status("202").send({
-                        mensaje: "El Usuario no existe"
+  connectionDb.query("SELECT * FROM administrator WHERE email_admin = ?", [emailUser], async (err, rows) => {
+    if (!err) {
+      if (rows.length > 0) {
+        let passwordUserDB = rows[0].password_admin;
+        let passwordCompare = await encrypted.matchPassword(passwordUser, passwordUserDB);
+        if (passwordCompare) {
+          let user = {
+            email: rows[0].email_admin,
+            permission: rows[0].id_permissions_admin
+          };
+          let accessToken = await jwt.generateAccessToken(user);
+          return res.status("200").send({
+            mensaje: "Usuario logueado con exito",
+            accessToken
+          });
+        } else {
+          return res.status("202").send({
+            mensaje: "Contraseña incorrecta"
+          });
+        }
+      } else if (rows.length === 0) {
+        await connectionDb.query("SELECT * FROM employee WHERE email_employee = ?", [emailUser], async (err, rows) => {
+          if (!err) {
+            if (rows.length > 0) {
+              let passwordUserDB = rows[0].password_employee;
+              let passwordCompare = await encrypted.matchPassword(passwordUser, passwordUserDB);
+              if (passwordCompare) {
+                let user = {
+                  email: rows[0].email_employee,
+                  permission: rows[0].id_permissions_employee
+                };
+                let accessToken = await jwt.generateAccessToken(user);
+                return res.status("200").send({
+                  mensaje: "Se logueo",
+                  accessToken
+                });
+              } else {
+                return res.status("202").send({
+                  mensaje: "Contraseña incorrecta"
+                });
+              }
+            } else if (rows.length === 0) {
+              await connectionDb.query("SELECT * FROM customer WHERE email_customer = ?", [emailUser], async (err, rows) => {
+                if (!err) {
+                  if (rows.length > 0) {
+                    let passwordUserDB = rows[0].password_customer;
+                    let passwordCompare = await encrypted.matchPassword(passwordUser, passwordUserDB);
+                    if (passwordCompare) {
+                      let user = {
+                        email: rows[0].email_customer,
+                        permission: rows[0].id_permissions_customer
+                      };
+                      let accessToken = await jwt.generateAccessToken(user);
+                      return res.status("200").send({
+                        mensaje: "Usuario logueado",
+                        accessToken
                       });
                     }
-                  } else {
+                  } else if (rows.length === 0) {
                     return res.status("202").send({
-                      mensaje: "Error al iniciar sesion",
-                      error: err
+                      mensaje: "El Usuario no existee"
                     });
                   }
+                } else {
+                  return res.status("202").send({
+                    mensaje: "Error al iniciar sesion",
+                    error: err
+                  });
                 }
-                );
               }
-            } else {
-              return res.status("202").send({
-                mensaje: "Error al iniciar sesion",
-                error: err
-              });
+              );
             }
+          } else {
+            return res.status("202").send({
+              mensaje: "Error al iniciar sesion",
+              error: err
+            });
           }
-          );
         }
-      } else if (err) {
-        return res.status("202").send({
-          mensaje: "Error al iniciar sesion",
-          error: err
-        });
+        );
       }
+    } else if (err) {
+      return res.status("202").send({
+        mensaje: "Error al iniciar sesion",
+      });
     }
+  }
   );
 };
 
