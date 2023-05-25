46ede07da2591c844de2bdf0662220536a82684b
Tatiana Barrera <barreratatiana709@gmail.com>
2023-05-24 21:55:23 -0500

    ltbm

--- src/controllers/cardShopping.controllers.js
+++ src/controllers/cardShopping.controllers.js
@@ -14,18 +14,18 @@ card.postShopping = async (req, res) => {
 
   connectionDB.query("SELECT id_customer FROM customer WHERE email_customer = ?", [idUser], (err, rows) => {
     let id = rows[0].id_customer
-    if (!err){
+    if (!err) {
       connectionDB.query("INSERT INTO cardshopping SET ?", {
         id_product: codeProduct,
         price_product: priceProduct,
         amount_Product: amountProduct,
         id_customer: id,
         id_store: codeStore,
-        name_product : name  
+        name_product: name
       }, (err, rows) => {
         if (!err) {
           connectionDB.query("SELECT * FROM cardshopping WHERE id_customer = ?", [id], (err, rows) => {
-            if (!err){
+            if (!err) {
               res.status(200).send({
                 message: "Productos encontrados",
                 data: rows
@@ -52,26 +52,31 @@ card.postShopping = async (req, res) => {
 
 card.getShopping = async (req, res) => {
   let idUser = req.user.idUser;
-
-  connectionDB.query("SELECT * FROM cardshopping WHERE id_customer = ?", [idUser], (err, rows) => {
-    if (!err){
-      res.status(200).send({
-        message: "Productos encontrados",
-        data: rows
-      })
-    } else {
-      res.status(500).send({
-        message: "Error al obtener los productos"
+  connectionDB.query("SELECT id_customer FROM customer WHERE email_customer = ?", [idUser], (err, rows) => {
+    let id = rows[0].id_customer
+    if (!err) {
+      connectionDB.query("SELECT * FROM cardshopping WHERE id_customer = ?", [id], (err, rows) => {
+        if (!err) {
+          res.status(200).send({
+            message: "Productos encontrados",
+            data: rows
+          })
+        } else {
+          res.status(500).send({
+            message: "Error al obtener los productos"
+          })
+        }
       })
     }
   })
 }
 
+
 card.deleteShopping = async (req, res) => {
   let codeProduct = req.params.idProduct;
   let idUser = req.user.idUser;
   connectionDB.query("DELETE FROM cardshopping WHERE id_product = ? AND id_customer = ?", [codeProduct, idUser], (err, rows) => {
-    if (!err){
+    if (!err) {
       res.status(200).send({
         message: "Producto eliminado del carrito"
       })
@@ -89,7 +94,7 @@ card.updateShopping = async (req, res) => {
   let codeStore = req.body.data.code;
   let amountProduct = req.bady.data.amount;
   connectionDB.query("UPDATE cardshopping SET amount_Product = ? WHERE id_product = ? AND id_customer = ? AND id_store = ?", [amountProduct, codeProduct, idUser, codeStore], (err, rows) => {
-    if (!err){
+    if (!err) {
       res.status(200).send({
         message: "Producto actualizado del carrito"
       })
